3de9f07a56af9439eaaece71f25069c4f7e4aa4d
Kowyo <kowyo@outlook.com>
2026-03-12 22:41:46 +0800

    feat: add multi-gpu training

--- train.sh
+++ train.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+export PYTORCH_ALLOC_CONF=expandable_segments:True
+rm -rf outputs/pi05_training
 
-uv run lerobot-train \
-    --dataset.repo_id=kowyo/plugin-v3 \
+LOG_FILE="train_$(date +%Y%m%d_%H%M%S).log"
+echo "Logging to $LOG_FILE"
+
+uv run accelerate launch --multi_gpu --num_processes=8 .venv/bin/lerobot-train \
+    --dataset.repo_id=kowyo/plugin-v4 \
+    --dataset.revision=main \
     --policy.type=pi05_custom \
     --output_dir=./outputs/pi05_training \
     --job_name=pi05_training \
@@ -11,9 +17,9 @@ uv run lerobot-train \
     --wandb.enable=true \
     --policy.dtype=bfloat16 \
     --policy.freeze_vision_encoder=false \
-    --policy.train_expert_only=false \
+    --policy.train_expert_only=true \
     --steps=3000 \
-    --policy.device=cuda \
-    --batch_size=32 \
+    --batch_size=8 \
     --policy.push_to_hub=true \
-    --policy.repo_id=kowyo/pi05-finetuned
+    --policy.repo_id=kowyo/pi05-finetuned \
+    2>&1 | tee "$LOG_FILE"
