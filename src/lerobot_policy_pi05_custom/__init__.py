02983c0739b9a64dffb499e6f313f79592358466
Kowyo <kowyo@outlook.com>
2026-03-18 13:03:29 +0800

    chore: rename policy filename

--- src/lerobot_policy_pi05_custom/__init__.py
+++ src/lerobot_policy_pi05_custom/__init__.py
@@ -1,8 +1,8 @@
 """Custom pi0.5 policy package for LeRobot."""
 
-from .configuration_pi05 import PI05Config
-from .modeling_pi05 import PI05Policy
-from .processor_pi05 import make_pi05_custom_pre_post_processors
+from .configuration_pi05_custom import PI05Config
+from .modeling_pi05_custom import PI05Policy
+from .processor_pi05_custom import make_pi05_custom_pre_post_processors
 
 __all__ = [
     "PI05Config",
